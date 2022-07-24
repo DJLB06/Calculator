537e43e44fa33aba3ac3c3e069d337ab9bd10a5a
Daniel Longmuir <djlongmuir@gmail.com>
2022-07-24 07:43:52 -0600

    add button query selectors, rearrange button layout

--- scripts.js
+++ scripts.js
@@ -34,3 +34,24 @@ function operate(number1,number2,operator){
    return value;
 }
 
+const displayBar = document.querySelector('#display-bar');
+
+const sevenButton = document.querySelector('.seven-button');
+const eightButton = document.querySelector('.eight-button');
+const nineButton = document.querySelector('.nine-button');
+const divideButton = document.querySelector('.divide-button');
+const fourButton = document.querySelector('.four-button');
+const fiveButton = document.querySelector('.five-button');
+const sixButton = document.querySelector('.six-button');
+const multiplyButton = document.querySelector('.multiply-button');
+const oneButton = document.querySelector('.one-button');
+const twoButton = document.querySelector('.two-button');
+const threeButton = document.querySelector('.three-button');
+const minusButton = document.querySelector('.minus-button');
+const zeroButton = document.querySelector('.zero-button');
+const decimalButton = document.querySelector('.decimal-button');
+const equalsButton = document.querySelector('.equals-button');
+const plusButton = document.querySelector('.plus-button');
+
+
+
